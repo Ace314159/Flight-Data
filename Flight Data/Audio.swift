5c0652231d22b9a9aab2c6f7db608b8e99b4ca84
Akash Munagala <akash.munagala@gmail.com>
2019-01-06 15:51:35 -0800

    Add working prototype of app

---
+++ Flight Data/Audio.swift
@@ -0,0 +1,166 @@
+//
+//  Audio.swift
+//  Flight Data
+//
+//  Created by ace on 12/23/18.
+//  Copyright © 2018 Ace. All rights reserved.
+//
+
+import Foundation
+import AudioUnit
+
+final class Audio: NSObject {
+    private var audioUnit: AudioUnit!
+    
+    // MARK: Audio Variables
+    let sampleRate: Float32 = 44100
+    let amplitude: Float32 = 1.0
+    let twoPI: Float32 = 2.0 * Float32.pi
+    
+    var time: Float32 = 0.0
+    let regFrequency: Float32 = 440
+    let alertFrequency: Float32 = 900
+    var frequency: Float32 = 0.0
+    let regInterval: Double = 0.8
+    var interval: Double = 0.2 // Length of quiet time
+    let playSoundLen: Double = 0.2
+    var playingSound = false
+    var prevPlayedTime: Double = 0.0
+    var playContinuous = false
+    private var muted = true
+    
+    var timeFactor: Double = 0
+    
+    override init() {
+        super.init()
+        // Calculate timeFactor
+        var tinfo = mach_timebase_info(numer: 0, denom: 0)
+        let error = mach_timebase_info(&tinfo)
+        assert(error == KERN_SUCCESS, "mach_timebase_info Failed")
+        timeFactor = Double(exactly: tinfo.numer)! / Double(exactly: tinfo.denom)!
+        
+        // Configure Audio Component
+        var defaultOutputDesc = AudioComponentDescription(componentType: kAudioUnitType_Output,
+                                                          componentSubType: kAudioUnitSubType_RemoteIO,
+                                                          componentManufacturer: kAudioUnitManufacturer_Apple,
+                                                          componentFlags: 0,
+                                                          componentFlagsMask: 0)
+        let defaultOutput = AudioComponentFindNext(nil, &defaultOutputDesc)!
+        
+        var err: OSStatus
+        
+        // Create a new instance of Audio Component
+        err = AudioComponentInstanceNew(defaultOutput, &audioUnit)
+        assert(err == noErr, "AudioComponentInstanceNew Failed: " + getError(Int(err)))
+        
+        // Set the render callback as the input for the Audio Component
+        var renderCallbackStruct = AURenderCallbackStruct(inputProc: renderCallback, inputProcRefCon: UnsafeMutableRawPointer(mutating: UnsafeRawPointer(Unmanaged.passUnretained(self).toOpaque())))
+        err = AudioUnitSetProperty(audioUnit,
+                                   kAudioUnitProperty_SetRenderCallback,
+                                   kAudioUnitScope_Input,
+                                   0,
+                                   &renderCallbackStruct,
+                                   UInt32(MemoryLayout<AURenderCallbackStruct>.size))
+        assert(err == noErr, "AudioUnitSetProperty SetRenderCallback Failed: " + getError(Int(err)))
+        
+        // Set the stream format
+        var streamFormat = AudioStreamBasicDescription(mSampleRate: Float64(sampleRate),
+                                                       mFormatID: kAudioFormatLinearPCM,
+                                                       mFormatFlags: kAudioFormatFlagsNativeFloatPacked|kAudioFormatFlagIsNonInterleaved,
+                                                       mBytesPerPacket: 4,
+                                                       mFramesPerPacket: 1,
+                                                       mBytesPerFrame: 4,
+                                                       mChannelsPerFrame: 1,
+                                                       mBitsPerChannel: 8 * UInt32(MemoryLayout<Float32>.size),
+                                                       mReserved: 0)
+        err = AudioUnitSetProperty(audioUnit,
+                                   kAudioUnitProperty_StreamFormat,
+                                   kAudioUnitScope_Input,
+                                   0,
+                                   &streamFormat,
+                                   UInt32(MemoryLayout<AudioStreamBasicDescription>.size))
+        assert(err == noErr, "AudioUintSetProperty StreamFormat Failed")
+    }
+    
+    func start() {
+        var err: OSStatus
+        err = AudioUnitInitialize(audioUnit)
+        assert(err == noErr, "AudioUnitInitialize Failed: " + getError(Int(err)))
+        err = AudioOutputUnitStart(audioUnit)
+        assert(err == noErr, "AudioOutputUnitStart Failed: " + getError(Int(err)))
+    }
+    
+    func isMuted() -> Bool {
+        return muted
+    }
+    
+    func mute() {
+        muted = true
+        playingSound = false
+        playContinuous = false
+    }
+    
+    func unmute() {
+        muted = false
+        playingSound = true
+        playContinuous = false
+        prevPlayedTime = 0.0
+    }
+    
+    deinit {
+        AudioOutputUnitStop(audioUnit)
+        AudioUnitUninitialize(audioUnit)
+    }
+    
+}
+
+private func renderCallback(inRefCon: UnsafeMutableRawPointer,
+                            ioActionFlags: UnsafeMutablePointer<AudioUnitRenderActionFlags>,
+                            inTimeStamp: UnsafePointer<AudioTimeStamp>,
+                            inBusNumber: UInt32,
+                            inNumberFrames: UInt32,
+                            ioData: UnsafeMutablePointer<AudioBufferList>?) -> OSStatus {
+    let audio = Unmanaged<Audio>.fromOpaque(inRefCon).takeUnretainedValue()
+    let abl = UnsafeMutableAudioBufferListPointer(ioData)
+    let buffer = abl![0]
+    let pointer: UnsafeMutableBufferPointer<Float32> = UnsafeMutableBufferPointer(buffer)
+    
+    if !audio.isMuted() {
+        if audio.playContinuous {
+            audio.playingSound = true
+        } else {
+            let time = audio.timeFactor * Double(inTimeStamp.pointee.mHostTime) / 1000000000
+            if audio.playingSound {
+                if time >= audio.prevPlayedTime + audio.playSoundLen {
+                    audio.playingSound = false
+                    audio.prevPlayedTime = time
+                } else {
+                    audio.playingSound = true
+                }
+            } else {
+                if time >= audio.prevPlayedTime + audio.interval {
+                    audio.playingSound = true
+                    audio.prevPlayedTime = time
+                } else {
+                    audio.playingSound = false
+                }
+            }
+        }
+    }
+    
+    for frame in 0..<inNumberFrames {
+        let pointerIndex = pointer.startIndex.advanced(by: Int(frame))
+        pointer[pointerIndex] = audio.playingSound ? sin(audio.time) * audio.amplitude : 0.0
+        audio.time += audio.twoPI * audio.frequency / audio.sampleRate
+    }
+    return noErr
+}
+
+private func getError(_ n: Int) -> String {
+    var s: String = String (UnicodeScalar((n >> 24) & 255)!)
+    s.append(Character(UnicodeScalar((n >> 16) & 255)!))
+    s.append(Character(UnicodeScalar((n >> 8) & 255)!))
+    s.append(Character(UnicodeScalar(n & 255)!))
+    
+    return s
+}
--- Flight Data/ViewController.swift
+++ Flight Data/ViewController.swift
@@ -6,15 +6,405 @@
 //  Copyright © 2018 Ace. All rights reserved.
 //
 
+import Foundation
 import UIKit
+import CoreLocation
+import CoreMotion
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate, CLLocationManagerDelegate, UITextFieldDelegate {
+    
+    // MARK: Labels
+    @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var altLabel: UILabel!
+    @IBOutlet weak var dAltLabel: UILabel!
+    @IBOutlet weak var speedTreshLabel: UILabel!
+    @IBOutlet weak var altTreshLabel: UILabel!
+    @IBOutlet weak var speedUnitsLabel: UILabel!
+    @IBOutlet weak var dAltUnitsLabel: UILabel!
+    @IBOutlet weak var altUnitsLabel: UILabel!
+    @IBOutlet weak var absAltLabel: UILabel!
+    // Placeholder Views
+    @IBOutlet weak var speedPlaceholder: UIView!
+    @IBOutlet weak var altPlaceholder: UIView!
+    // Positioning Constraints
+    @IBOutlet weak var speedUnitsPos: NSLayoutConstraint!
+    @IBOutlet weak var altUnitsPos: NSLayoutConstraint!
+    @IBOutlet weak var dAltUnitsPos: NSLayoutConstraint!
+    // Mute Button
+    @IBOutlet weak var muteBtn: UIImageView!
+    @IBOutlet weak var muteBtnSize: NSLayoutConstraint!
+    // Alt Buttons
+    @IBOutlet weak var zeroBtn: UIButton!
+    @IBOutlet weak var thousandBtn: UIButton!
+    
+    
+    var speed = -1.0
+    var relAlt = -1.0
+    var altOffset = 0.0
+    var alts: [Double] = [0.0]
+    var times: [Double] = [CACurrentMediaTime()]
+    var dAlt = -1.0
+    
+    // MARK: Tresholds
+    var speedTresh = 0.0
+    var altTresh = 0.0
+    let changeIntervalTresh = 30.0
+    
+    // MARK: Audio
+    let audio = Audio()
+    let freqDiffFactor = 1.0
+    lazy var intervalFactor = audio.regInterval / changeIntervalTresh
 
+    let locationManager = CLLocationManager()
+    let altimeter = CMAltimeter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        speedLabel.addGestureRecognizer(UILongPressGestureRecognizer(target: self, action: #selector(self.setSpeedTresh(_:))))
+        altLabel.addGestureRecognizer(UILongPressGestureRecognizer(target: self, action: #selector(self.setAltTresh(_:))))
+        
+        muteBtn.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.muteToggle(_:))))
+        
+        zeroBtn.tag = 0
+        zeroBtn.addTarget(self, action: #selector(self.setCurrentAlt(_:)), for: .touchUpInside)
+        thousandBtn.tag = 1000
+        thousandBtn.addTarget(self, action: #selector(self.setCurrentAlt(_:)), for: .touchUpInside)
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        adjustFonts()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            switch CLLocationManager.authorizationStatus() {
+            case .notDetermined:
+                locationManager.requestWhenInUseAuthorization()
+            case .restricted, .denied:
+                alert("Location Services Required", "Please enable location services for this app in Settings.")
+            case .authorizedAlways, .authorizedWhenInUse:
+                print("Access Allowed")
+            }
+            
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.distanceFilter = kCLDistanceFilterNone
+            locationManager.delegate = self
+            locationManager.startUpdatingLocation()
+        } else {
+            alert("Location Serivces Not Enabled", "Plse enable location services.")
+            return
+        }
+        
+        if !CMAltimeter.isRelativeAltitudeAvailable() {
+            alert("Relative Altitude Not Available", "This device does not have an altimeter.")
+            return
+        }
+        
+        altimeter.startRelativeAltitudeUpdates(to: OperationQueue.main, withHandler: CMAltitudeHandler)
+        
+        mute()
+        audio.start()
+    }
+    
+    @objc func setSpeedTresh(_ gesture: UILongPressGestureRecognizer) {
+        if gesture.state != .began { return }
+        
+        let alert = UIAlertController(title: "Set Minimum Speed", message: "Alert below minimum speed", preferredStyle: .alert)
+        
+        alert.addTextField { (textField: UITextField!) in
+            textField.placeholder = "Minimum Speed"
+            textField.keyboardType = .numberPad
+            textField.delegate = self
+        }
+        let prevMuted = audio.isMuted()
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alert.addAction(UIAlertAction(title: "Set", style: .default, handler: { (_) in
+            let textField = alert.textFields![0] as UITextField
+            if textField.text != "" {
+                let tresh = Double(textField.text!)!
+                self.confirm("Confirm Minimum Speed", String(format: "Are you sure you want to set the minimum speed to %.0f knots?", tresh), { (_) in
+                    self.speedTresh = tresh >= 0 ? tresh : self.speedTresh
+                    if self.speedTresh >= 0 {
+                        self.speedTreshLabel.text = String(format: "Warn @ %.0f", self.speedTresh)
+                    }
+                    print("Updating Speed Treshold:", self.speedTresh)
+                    self.updateAudioFreq()
+                })
+                
+            }
+            
+        }))
+        
+        mute()
+        self.present(alert, animated: true, completion: { () in
+            if !prevMuted {
+                self.unmute()
+            }
+        })
+    }
+    
+    @objc func setAltTresh(_ gesture: UILongPressGestureRecognizer) {
+        if gesture.state != .began { return }
+        
+        let alert = UIAlertController(title: "Target Altitude", message: "Set target altitude", preferredStyle: .alert)
+        
+        /*alert.addTextField { (textField: UITextField!) in
+            textField.placeholder = "Current Altitude"
+            textField.keyboardType = .numberPad
+            textField.delegate = self
+        }*/
+        alert.addTextField { (textField: UITextField!) in
+            textField.placeholder = "Target Altitude"
+            textField.keyboardType = .numberPad
+            textField.delegate = self
+        }
+        let prevMuted = audio.isMuted()
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alert.addAction(UIAlertAction(title: "Set", style: .default, handler: { (_) in
+            /*if (alert.textFields![0] as UITextField).text != "" {
+                let currentAlt = Double((alert.textFields![0] as UITextField).text!)!
+                self.altOffset = currentAlt - self.relAlt
+                print("Updating Altitude Offset:", self.altOffset)
+            }*/
+            if (alert.textFields![0] as UITextField).text != "" {
+                let tresh = Double((alert.textFields![0] as UITextField).text!)!
+                self.confirm("Confirm Target Altitude", String(format: "Are you sure you want to set the target altitude as %.0f ft?", tresh), { (_) in
+                    self.altTresh = tresh
+                    if self.altTresh >= 0 {
+                        self.altTreshLabel.text = String(format: "Target @ %.0f", self.altTresh)
+                    }
+                    print("Updating Altitude Treshold:", self.altTresh)
+                })
+            }
+            self.updateAltLabels(self.relAlt)
+            self.updateAudioInterval()
+        }))
+        
+        self.mute()
+        self.present(alert, animated: true, completion: { () in
+            if !prevMuted {
+                self.unmute()
+            }
+        })
+    }
+    
+    @objc func setCurrentAlt(_ sender: UIButton!) {
+        let currentAlt = Double(sender.tag)
+        self.altOffset = currentAlt - self.relAlt
+        print("Updating Altitude Offset:", self.altOffset)
+        
+        self.updateAltLabels(self.relAlt)
+        self.updateAudioInterval()
+    }
+    
+    @objc func muteToggle(_ gesture: UITapGestureRecognizer) {
+        if gesture.state != .ended { return }
+        
+        if audio.isMuted() {
+            unmute()
+        } else {
+            mute()
+        }
+    }
+    
+    func unmute() {
+        audio.unmute()
+        muteBtn.image = UIImage(named: "unmuted")
+        self.updateAudioFreq()
+        self.updateAudioInterval()
+    }
+    
+    func mute() {
+        audio.mute()
+        muteBtn.image = UIImage(named: "muted")
+    }
+    
+    func setTresh(_ title: String, _ message: String, _ placeholders: Array<String>, _ callback: @escaping (Double) -> Void) {
+        
+    }
+    
+    func updateAudioFreq() {
+        if speed >= 0 && speed < speedTresh {
+            audio.frequency = audio.alertFrequency + Float(freqDiffFactor * (speedTresh - speed))
+            print("Updaing Frequency: Alert", audio.frequency)
+        } else {
+            audio.frequency = audio.regFrequency
+            print("Updating Frequency: Regular", audio.frequency)
+        }
+    }
+    
+    func updateAudioInterval() {
+        let d = relAlt + altOffset - altTresh
+        if d <= changeIntervalTresh {
+            audio.interval = intervalFactor * d
+            if d <= 3 {
+                audio.playContinuous = true
+            } else {
+                audio.playContinuous = false
+            }
+        } else {
+            audio.playContinuous = false
+            audio.interval = audio.regInterval
+        }
+        print("Updating Audio Interval:", audio.interval)
+    }
+    
+    func updateAltLabels(_ newRelAlt: Double) {
+        let curTime = CACurrentMediaTime()
+        relAlt = newRelAlt
+        alts.append(relAlt)
+        times.append(curTime)
+        if times.last! - times.first! >= 3 {
+            dAlt = (alts.last! - alts.first!) / (times.last! - times.first!) * 60
+            alts.removeFirst()
+            times.removeFirst()
+        }
+        
+        var alt = (relAlt + altOffset).rounded()
+        if alt.sign == .minus && alt == 0 {
+            alt = 0
+        }
+        let sign = alt.sign == .plus ? "+" : ""
+        
+        var d = (50 * (dAlt / 50).rounded())
+        if d.sign == .minus && d == 0 {
+            d = 0
+        }
+        let color = d >= 0 ? UIColor.black : UIColor.blue
+        
+        altLabel.text = String(format: "\(sign)%.0f", alt)
+        dAltLabel.text = String(format: "%.0f", d)
+        dAltLabel.textColor = color
+    }
+    
+    func CMAltitudeHandler(data: CMAltitudeData?, error: Error?)  {
+        if error != nil {
+            print("Altitude Error", error!.localizedDescription)
+            return
+        }
+        
+        updateAltLabels(Double(exactly: data!.relativeAltitude)! * 3.28084)
+        updateAudioInterval()
+        
+        print("Updating Relative Altitude:", relAlt)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let speedRaw = manager.location?.speed else { print("Invalid Speed"); return }
+        guard let altRaw = manager.location?.altitude else { print ("Invalid Altitude"); return }
+        speed = speedRaw * 1.94384
+        let absAlt = altRaw * 3.28084
+        print("Updating Speed:", speed)
+        print("Updating Absolute Altitude:", absAlt)
+        
+        if speed.sign == .minus {
+            speedLabel.text = "-"
+        } else {
+            speedLabel.text = String(format: "%.0f", speed.rounded())
+        }
+        
+        updateAudioFreq()
+        
+        absAltLabel.text = String(format: "MSL @ %.0f", absAlt)
+    }
+    
+    func adjustFonts() {
+        speedLabel.fitTextToHeight(speedPlaceholder.frame.height * 0.8)
+        speedUnitsPos.constant = speedLabel.font.descender + 8
+        speedUnitsLabel.fitTextToHeight(speedPlaceholder.frame.height * 0.1)
+        speedTreshLabel.fitTextToHeight(speedPlaceholder.frame.height * 0.1)
+        
+        altLabel.fitTextToHeight(altPlaceholder.frame.height * 0.7)
+        altUnitsPos.constant = altLabel.font.descender + 8
+        altUnitsLabel.fitTextToHeight(altPlaceholder.frame.height * 0.1)
+        altTreshLabel.fitTextToHeight(altPlaceholder.frame.height * 0.1)
+        absAltLabel.fitTextToHeight(altPlaceholder.frame.height * 0.1)
+        
+        dAltLabel.fitTextToHeight(altPlaceholder.frame.height * 0.7)
+        dAltUnitsPos.constant = dAltLabel.font.descender + 8
+        dAltUnitsLabel.fitTextToHeight(altPlaceholder.frame.height * 0.1)
+        
+        muteBtnSize.constant = self.view.frame.height * 0.1
+        
+        zeroBtn.titleLabel!.fitTextToHeight(altPlaceholder.frame.height * 0.1)
+        thousandBtn.titleLabel!.fitTextToHeight(altPlaceholder.frame.height * 0.1)
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let allowedCharacters = CharacterSet.decimalDigits
+        let characterSet = CharacterSet(charactersIn: string)
+        return allowedCharacters.isSuperset(of: characterSet)
+    }
+    
+    
+    func alert(_ title: String, _ message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func confirm(_ title: String, _ message: String, _ handler: @escaping (UIAlertAction) -> Void) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alert.addAction(UIAlertAction(title: "Confirm", style: .default, handler: handler))
+        
+        mute()
+        self.present(alert, animated: true)
+    }
 }
 
+
+
+extension UIFont {
+    
+    /**
+     Will return the best font conforming to the descriptor which will fit in the provided bounds.
+     */
+    static func bestFittingFontSize(for text: String, in bounds: CGRect, fontDescriptor: UIFontDescriptor, additionalAttributes: [NSAttributedString.Key: Any]? = nil, height: CGFloat) -> CGFloat {
+        var attributes = additionalAttributes ?? [:]
+        
+        let infiniteBounds = CGSize(width: CGFloat.infinity, height: CGFloat.infinity)
+        var bestFontSize: CGFloat = height
+        
+        for fontSize in stride(from: bestFontSize, through: 0, by: -1) {
+            let newFont = UIFont(descriptor: fontDescriptor, size: fontSize)
+            attributes[.font] = newFont
+            
+            let currentFrame = text.boundingRect(with: infiniteBounds, options: [.usesLineFragmentOrigin, .usesFontLeading], attributes: attributes, context: nil)
+            
+            if currentFrame.height <= height {
+                bestFontSize = fontSize
+                break
+            }
+        }
+        return bestFontSize
+    }
+    
+    static func bestFittingFont(for text: String, in bounds: CGRect, fontDescriptor: UIFontDescriptor, additionalAttributes: [NSAttributedString.Key: Any]? = nil, height: CGFloat) -> UIFont {
+        let bestSize = bestFittingFontSize(for: text, in: bounds, fontDescriptor: fontDescriptor, additionalAttributes: additionalAttributes, height: height)
+        return UIFont(descriptor: fontDescriptor, size: bestSize)
+    }
+}
+
+extension UILabel {
+    func fitTextToHeight(_ height: CGFloat) {
+        guard let text = text, let currentFont = font else { return }
+        
+        let bestFittingFont = UIFont.bestFittingFont(for: text, in: bounds, fontDescriptor: currentFont.fontDescriptor, additionalAttributes: basicStringAttributes, height: height)
+        font = bestFittingFont
+    }
+    
+    private var basicStringAttributes: [NSAttributedString.Key: Any] {
+        var attribs = [NSAttributedString.Key: Any]()
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = self.textAlignment
+        paragraphStyle.lineBreakMode = self.lineBreakMode
+        attribs[.paragraphStyle] = paragraphStyle
+        
+        return attribs
+    }
+}
